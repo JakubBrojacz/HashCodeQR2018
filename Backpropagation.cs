ae22b10f7050ea62685d45ea56563ac8b57dd1c5
Jakub Brojacz <38508437+JakubBrojacz@users.noreply.github.com>
2019-02-07 16:15:00 +0100

    No stack overflow anymore
    Just a bunch of goto statements xd

--- Backpropagation.cs
+++ Backpropagation.cs
@@ -9,15 +9,14 @@ namespace Self_driving_rides
     {
         public static int Task(string path)
         {
-            List<Ride> rides;
-            Board board;
-            Input.ReadData(path, out rides, out board);
-            foreach (var item in rides)
+            List<Ride> ridesList;
+            Input.ReadData(path, out ridesList, out board);
+            foreach (var item in ridesList)
             {
                 item.t = Tools.InfiniteMetric(item.a, item.b, item.x, item.y);
             }
 
-            List<Car> cars = new List<Car>();
+            List<Car> carsList = new List<Car>();
             for (int i = 0; i < board.F; i++)
             {
                 Car car = new Car();
@@ -25,17 +24,30 @@ namespace Self_driving_rides
                 car.x = 0;
                 car.y = 0;
                 car.t = 0;
-                cars.Add(car);
+                carsList.Add(car);
             }
 
-            rides.Sort((x, y) => (x.s.CompareTo(y.s)));
+            ridesList.Sort((x, y) => (x.s.CompareTo(y.s)));
+            rides = ridesList.ToArray();
+            max_ride = ridesList.Count;
+            {
+                Car sentiel = new Car();
+                sentiel.i = -1;
+                sentiel.x = -1;
+                sentiel.y = -1;
+                sentiel.t = board.T+1;
+                carsList.Add(sentiel);
+            }
+            cars = carsList.ToArray();
+            max_car = carsList.Count - 1;
+            cleanups = new RideCleanup[ridesList.Count];
 
             //rides.PrintList(r => r.i.ToString());
-            cant_be_better = rides.Sum(r => r.t + board.B);
+            cant_be_better = ridesList.Sum(r => r.t + board.B);
             global_max = 0;
             already_lost = 0;
-            SolveRecurrent(board, rides.ToArray(), 0, rides.Count, cars.ToArray(), 0, cars.Count, false, 0);
-            SolveRecurrent(board, rides.ToArray(), 0, rides.Count, cars.ToArray(), 0, cars.Count, true, 0);
+            SolveRecurrent(0, 0, false, 0);
+            SolveRecurrent(0, 0, true, 0);
             Console.WriteLine("points: {0}", global_max);//
             //Console.ReadKey();
             return global_max;
@@ -45,75 +57,114 @@ namespace Self_driving_rides
         static int cant_be_better;
         static int global_max;
         static List<int> assigment;
-
-
-        public static void SolveRecurrent(Board board, Ride[] rides, int actual_ride,int max_ride, Car[] cars,int actual_car,int max_car,bool dismiss,int actual_score)
+        struct RideCleanup
         {
-            //if(dismiss)
-            //    Console.WriteLine($"Exx true {actual_ride}");
-            //else
-            //    Console.WriteLine($"Exx {actual_car} {actual_ride}");
-            int points_for_ride = 0;
-            int past_t=0;
-            int past_x=0;
-            int past_y=0;
-            int temporary_lost = 0;
-            if(dismiss)
+            public int points_for_ride;
+            public int past_t;
+            public int past_x;
+            public int past_y;
+            public int temporary_lost;
+            public int actual_car;
+            public bool dismiss;
+            public int i;
+            public void zero(bool dis, int car)
             {
-                temporary_lost += rides[actual_ride].t + board.B;
+                points_for_ride = 0;
+                past_t = 0;
+                past_x = 0;
+                past_y = 0;
+                temporary_lost = 0;
+                dismiss = dis;
+                actual_car = car;
+            }
+        }
+        static RideCleanup[] cleanups;
+        static Board board;
+        static Ride[] rides;
+        static Car[] cars;
+        static int max_ride;
+        static int max_car;
+
+
+
+        public static void SolveRecurrent(int actual_ride,int actual_car,bool dismiss,int actual_score)
+        {
+        Start:
+            cleanups[actual_ride].zero(dismiss,actual_car);
+            if (cleanups[actual_ride].dismiss)
+            {
+                cleanups[actual_ride].temporary_lost += rides[actual_ride].t + board.B;
                 //rides[actual_ride].car = null;
             }
             else
             {
-                var c = cars[actual_car];
+                var c = cars[cleanups[actual_ride].actual_car];
                 var r = rides[actual_ride];
                 if (c.WhenCanYouGetThere(r.a, r.b) + r.t > r.f)
                 {
                     //Console.WriteLine($"Dupa {actual_car} {actual_ride}");
-                    return;
+                    actual_ride--;
+                    goto Load;
                 }
                 c.schedule.Add(r);
                 if (c.WhenCanYouGetThere(r.a, r.b) <= r.s)
-                    points_for_ride += board.B;
+                    cleanups[actual_ride].points_for_ride += board.B;
                 else
-                    temporary_lost += board.B;
-                points_for_ride += r.t;
-                past_t = c.t;
-                past_x = c.x;
-                past_y = c.y;
+                    cleanups[actual_ride].temporary_lost += board.B;
+                cleanups[actual_ride].points_for_ride += r.t;
+                cleanups[actual_ride].past_t = c.t;
+                cleanups[actual_ride].past_x = c.x;
+                cleanups[actual_ride].past_y = c.y;
                 c.t = c.WhenCanYouGetThere(r.a, r.b) + r.t;
                 c.x = r.x;
                 c.y = r.y;
+
+                actual_score += cleanups[actual_ride].points_for_ride;
             }
+            already_lost += cleanups[actual_ride].temporary_lost;
+
+
+            cleanups[actual_ride].i = -1;
+        Load:
+            //the only return statement
+            if (actual_ride < 0)
+                return;
 
             //check if it is even possible to improve max
-            already_lost += temporary_lost;
             if (cant_be_better - already_lost <= global_max)
                 goto Cleanup;
 
-            if(actual_ride<max_ride-1)
+            //all cars were calculated
+            if (cleanups[actual_ride].i == max_car)
+                goto Cleanup;
+
+            //recurrent
+            if (actual_ride<max_ride-1)
             {
-                int tmp_x = -1;
-                int tmp_y = -1;
-                int tmp_t = -1;
-                for(int i=0;i<max_car;i++)
+            SkipCar:
+                cleanups[actual_ride].i+=1;
+                actual_car = cleanups[actual_ride].i;
+                if (cleanups[actual_ride].i <max_car)
                 {
-                    if(cars[i].x!=tmp_x || cars[i].y!=tmp_y || cars[i].t!=tmp_t)
-                    {
-                        SolveRecurrent(board, rides, actual_ride + 1, max_ride, cars, i, max_car, false, actual_score + points_for_ride);
-                        tmp_x = cars[i].x;
-                        tmp_y = cars[i].y;
-                        tmp_t = cars[i].t;
-                    }
+                    if (cars[cleanups[actual_ride].i].x == cars[cleanups[actual_ride].i + 1].x && cars[cleanups[actual_ride].i].y == cars[cleanups[actual_ride].i + 1].y && cars[cleanups[actual_ride].i].t >= cars[cleanups[actual_ride].i + 1].t)
+                        goto SkipCar;
+                    //SolveRecurrent(actual_ride + 1, cleanups[actual_ride].i, false, actual_score + cleanups[actual_ride].points_for_ride);
+                    dismiss = false;
+                }else
+                {
+                    //SolveRecurrent(actual_ride + 1, 0, true, actual_score + cleanups[actual_ride].points_for_ride);
+                    dismiss = true;
                 }
-                SolveRecurrent(board, rides, actual_ride + 1, max_ride, cars, 0, max_car, true, actual_score + points_for_ride);
+                actual_ride++;
+                goto Start;
+                
             }
             else
             {
-                //Console.WriteLine($"Score: {actual_score}");
-                if (actual_score + points_for_ride > global_max)
+                Console.WriteLine($"Score: {actual_score}");
+                if (actual_score > global_max)
                 {
-                    global_max = actual_score + points_for_ride;
+                    global_max = actual_score;
                     assigment = new List<int>(board.N + board.F * 2);
                     foreach (var c in cars)
                     {
@@ -127,15 +178,18 @@ namespace Self_driving_rides
 
         Cleanup:
             //return to initial state 
-            already_lost -= temporary_lost;
-            if (!dismiss)
+            already_lost -= cleanups[actual_ride].temporary_lost;
+            actual_score -= cleanups[actual_ride].points_for_ride;
+            if (!cleanups[actual_ride].dismiss)
             {
-                var c = cars[actual_car];
+                var c = cars[cleanups[actual_ride].actual_car];
                 c.schedule.RemoveAt(c.schedule.Count - 1);
-                c.t = past_t;
-                c.x = past_x;
-                c.y = past_y;
+                c.t = cleanups[actual_ride].past_t;
+                c.x = cleanups[actual_ride].past_x;
+                c.y = cleanups[actual_ride].past_y;
             }
+            actual_ride--;
+            goto Load;
         }
     }
 }
